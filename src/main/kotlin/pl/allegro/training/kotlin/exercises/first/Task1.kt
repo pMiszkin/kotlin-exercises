1b22b811f0faf5e1ba51e3b95855a2602b3556d7
Konrad Kaminski <konrad.kaminski@allegrogroup.com>
2017-11-21 11:10:10 +0100

    Task documentation fix.

--- src/main/kotlin/pl/allegro/training/kotlin/exercises/first/Task1.kt
+++ src/main/kotlin/pl/allegro/training/kotlin/exercises/first/Task1.kt
@@ -2,8 +2,8 @@ package pl.allegro.training.kotlin.exercises.first
 
 /**
  * Define a final class `MyError` with a constructor that has the following parameters:
- * * `code` of nullable type `String` - with no default value
- * * `message` of type `String` - with no default value
+ * * `code` of type `String` - with no default value
+ * * `message` of nullable type `String` - with no default value
  * * `httpStatus` of type `Int` - with the default value of 500
  *
  * Define a `toString` function which return the `httpStatus` and `message` separated by a colon, e.g.
