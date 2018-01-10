424cf6c72c8038751db1e00ba89714c627127252
Konrad Kaminski <konrad.kaminski@allegrogroup.com>
2018-01-10 22:32:28 +0100

    Moved to multi-module.

---
+++ first/src/test/kotlin/pl/allegro/training/kotlin/exercises/first/Task1Test.kt
@@ -0,0 +1,69 @@
+package pl.allegro.training.kotlin.exercises.first
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import kotlin.reflect.full.createType
+
+/*
+internal class Task1Test {
+
+    @Test
+    fun `should return proper string for both message and httpStatus given`() {
+        //given
+        val error = MyError("CRITICAL_ERROR", "Something went wrong", 503)
+
+        //when
+        val result = error.toString()
+
+        //then
+        assertEquals("503:Something went wrong", result)
+    }
+
+    @Test
+    fun `should return proper string for null message and given httpStatus`() {
+        //given
+        val error = MyError("NOT_FOUND", null, 404)
+
+        //when
+        val result = error.toString()
+
+        //then
+        assertEquals("404:null", result)
+    }
+
+    @Test
+    fun `should return proper string for given message and default httpStatus`() {
+        //given
+        val error = MyError("CRITICAL_ERROR", "Something went wrong")
+
+        //when
+        val result = error.toString()
+
+        //then
+        assertEquals("500:Something went wrong", result)
+    }
+
+    @Test
+    fun `should have proper constructor signature`() {
+        assertEquals(1, MyError::class.constructors.size)
+
+        val constructor = MyError::class.constructors.first()
+        assertTrue(constructor.parameters.size == 3)
+
+        val nonNullableStringType = String::class.createType(nullable = false)
+        val nullableStringType = String::class.createType(nullable = true)
+        val nonNullableIntType = Int::class.createType(nullable = false)
+
+        assertFalse(constructor.parameters[0].isOptional)
+        assertTrue(constructor.parameters[0].type == nonNullableStringType)
+
+        assertFalse(constructor.parameters[1].isOptional)
+        assertTrue(constructor.parameters[1].type == nullableStringType)
+
+        assertTrue(constructor.parameters[2].isOptional)
+        assertTrue(constructor.parameters[2].type == nonNullableIntType)
+    }
+}
+*/
---
+++ first/src/test/kotlin/pl/allegro/training/kotlin/exercises/first/Task2Test.kt
@@ -0,0 +1,61 @@
+package pl.allegro.training.kotlin.exercises.first
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+/*
+internal class Task2Test {
+
+    @Test
+    fun `should parse proper date`() {
+        val date = parseDate("1986-04-26")
+
+        assertEquals(date.year, 1986)
+        assertEquals(date.month, 4)
+        assertEquals(date.day, 26)
+    }
+
+    @Test
+    fun `should throw when date has invalid format`() {
+        assertThrows(IllegalArgumentException::class.java, { parseDate("1986/04/26") })
+    }
+
+    @Test
+    fun `should throw when month is out of range`() {
+        assertThrows(IllegalArgumentException::class.java, { parseDate("1986/13/26")})
+    }
+
+    @Test
+    fun `should throw when day is out of range`() {
+        assertThrows(IllegalArgumentException::class.java, { parseDate("1986-04-31")})
+    }
+
+    @Test
+    fun `should proper handle leap year`() {
+        val date = parseDate("2000-02-29")
+
+        assertEquals(date.year, 2000)
+        assertEquals(date.month, 2)
+        assertEquals(date.day, 29)
+    }
+
+    @Test
+    fun `after method should return true for date1 being after date2`() {
+        val date1 = parseDate("1986-04-27")
+        val date2 = parseDate("1986-04-26")
+
+        assertTrue(date1 after date2)
+    }
+
+    @Test
+    fun `after method should return false for date1 after date2`() {
+        val date1 = parseDate("1986-04-26")
+        val date2 = parseDate("1986-04-27")
+
+        assertFalse(date1 after date2)
+    }
+}
+*/
