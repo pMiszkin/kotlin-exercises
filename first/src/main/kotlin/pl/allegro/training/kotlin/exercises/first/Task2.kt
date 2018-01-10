08fa56ff3e046b2da5ccd596817c1ad7dee39df6
Konrad Kaminski <konrad.kaminski@allegrogroup.com>
2018-01-11 00:15:02 +0100

    Small fixes.

--- first/src/main/kotlin/pl/allegro/training/kotlin/exercises/first/Task2.kt
+++ first/src/main/kotlin/pl/allegro/training/kotlin/exercises/first/Task2.kt
@@ -1,7 +1,5 @@
 package pl.allegro.training.kotlin.exercises.first
 
-import pl.allegro.training.kotlin.exercises.first.maksymalna_liczba_dni_w_miesiacu_danego_roku as maxDaysInMonth
-
 /**
  * Create implementation of the `parseDate` function - the date will be
  * provided in yyyy-mm-dd format. You should use `DATE_REGEX` to parse given string.
