4cee3b3a354f9d8bf09686ecfa5e9ac0200b1663
Konrad Kaminski <konrad.kaminski@allegrogroup.com>
2018-01-11 15:42:05 +0100

    Fix for task description.

--- third/src/main/kotlin/pl/allegro/training/kotlin/exercises/third/Task3.kt
+++ third/src/main/kotlin/pl/allegro/training/kotlin/exercises/third/Task3.kt
@@ -7,10 +7,9 @@ package pl.allegro.training.kotlin.exercises.third
  *
  * Define a val with the name `scorer` which holds a scoring function with the following scoring algorithm:
  * if (income < 100_000) 0 points
- * else +1 point for every 10_000 income above 100_000 (e.g. 1 point for 115_000)
- * +
  * if (age < 18) 0 points
- * else + 1 point if 18 <= age < 25
+ * else +1 point for every 10_000 income above 100_000 (e.g. 1 point for 115_000)
+ *  and + 1 point if 18 <= age < 25
  *      + 2 points if 25 <= age < 35
  *      + 3 point if 35 <= age < 50
  *      + 2 points if 50 <= age < 65
