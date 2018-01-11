486556e47ea91e33ce2587d6f1eaeea2b920b411
Konrad Kaminski <konrad.kaminski@allegrogroup.com>
2018-01-11 11:34:22 +0100

    Fix for task description.

--- first/src/main/kotlin/pl/allegro/training/kotlin/exercises/first/Task3.kt
+++ first/src/main/kotlin/pl/allegro/training/kotlin/exercises/first/Task3.kt
@@ -5,7 +5,7 @@ package pl.allegro.training.kotlin.exercises.first
  * * `EchoProcess<T>: Process<T, T>` - the `run` method should simply return the `input` argument,
  * * `ShredProcess<A>: Process<A, Unit>` - the `run` method should check if the string representation of input
  *   (i.e. the value returned by calling `toString`) contains "TOP SECRET" and if it's not it should throw [IllegalArgumentException],
- * * `EventStreamProcess(output: B): Process<A, Unit>` - the `run` method should always return output given in constructor.
+ * * `EventStreamProcess(output: B): Process<Unit, B>` - the `run` method should always return output given in constructor.
  */
 
 interface Process<in A, out B> {
