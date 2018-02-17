4776cc981019be0efebebbfdec23237a425e9391
Konrad Kaminski <konrad.kaminski@allegrogroup.com>
2018-02-17 11:40:25 +0100

    Test fix.

--- first/src/test/kotlin/pl/allegro/training/kotlin/exercises/first/Task2Test.kt
+++ first/src/test/kotlin/pl/allegro/training/kotlin/exercises/first/Task2Test.kt
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.util.Random
 
 /*
 internal class Task2Test {
